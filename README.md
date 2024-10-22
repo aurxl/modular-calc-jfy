15a6aff7204ab2ca320c964fc08fc4204aba0b88
Jan Meineke <jan.meineke@mailbox.org>
2024-10-22 08:31:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Rechnermodul 'JustForYou'
+Projektteilnehmer:
+- Sarah Zimmermann
+- Kenny Schilde
+- Tommy Pahlitzsch
+- Jan Meineke
+
---
+++ shell.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    nativeBuildInputs = with pkgs; [
+      pandoc
+      poetry
+      python312
+    ];
+
+  shellHook = ''
+    export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
+    poetry env use $(which python)
+    source $(poetry env info --path)/bin/activate
+  '';
+
+}
