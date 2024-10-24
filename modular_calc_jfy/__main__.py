b053399b6d71f1c722d60c53d9cca2d6bb3a4db9
Jan Meineke <jan.meineke@mailbox.org>
2024-10-24 10:42:12 +0200

    adding basic main window setup

---
+++ modular_calc_jfy/__main__.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+"""
+Copyright (C) 2024 
+- Sarah Zimmermann
+- Kenny Schilde
+- Tommy Pahlitsch
+- Jan Meineke <jan.meineke@traceronic.de>
+
+All rights reserved.
+
+The modification and distribution of this software is
+hereby not permitted, unless otherwise communicated by the
+offial publisher.
+
+The use of this software is only permitted when distributed
+by the official publisher or one of it's offical distributors.
+
+--------------------------------------------------------------
+
+This software is part of the modular calculator 'JustForYou',
+a software project for compter students at BSZET Dresden.
+"""
+
+from PyQt6 import QtWidgets, uic
+from pathlib import Path
+
+import sys
+
+
+UI_FILE = f"{Path(__file__).parent.resolve()}/window.ui"
+
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__(None)
+        uic.loadUi(UI_FILE, self)
+         
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    app.exec()
+
+if __name__ == "__main__":
+    main()
