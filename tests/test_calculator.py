808e1078fdf8580b3842e41c5ef7fd0d17e519a0
Jan Meineke <jan.meineke@mailbox.org>
2025-01-23 12:11:12 +0100

    adding some more calc tests and fix floating numbers in calc

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,6 @@
 import unittest
 
-from modular_calc_jfy.calculator import Calculator
+from modular_calc_jfy.calculator import Calculator, InvalidExpressionError
 
 class Test_Calculator(unittest.TestCase):
 
@@ -10,20 +10,32 @@ class Test_Calculator(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_calc_simple_addition(self):
+    def test_calc_simple_calculations(self):
         test_cases = [
-                ("1+1", "2"), 
-                ("5+2", "7"),
-                ("1165+23445", "24610")
-                ]
+                ("1+1", 2.0), 
+                ("5.3+2.1", 7.4),
+                ("1165+23445", 24610),
+                ("3+3+3", 9),
+                ("42-42", 0),
+                ("42+8-25", 25),
+                ("3*3", 9),
+                ("(15-5)*3", 30),
+                ("25/5", 5),
+                ("(100/4)*5", 125)
+            ]
 
         for test_case in test_cases:
             self.assertEqual(self.calculator.calc(test_case[0]), test_case[1])
 
     def test_calc_invalid_input(self):
-        pass
+        test_cases = [
+                "(1*a)+5=2",
+                "[foo]bar",
+                "'1+1'",
+            ]
 
-    def test_calc_multiple_addition(self):
-        pass
+        with self.assertRaises(InvalidExpressionError):
+            for test_case in test_cases:
+                self.calculator.calc(test_case)
 
 
