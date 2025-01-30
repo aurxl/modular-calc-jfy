f1a09ed5666e7e2c6a82d6d8406570b84d135286
Jan Meineke <jan.meineke@mailbox.org>
2025-01-30 11:11:07 +0100

    fix invalid list items

--- tests/test_percentage.py
+++ tests/test_percentage.py
@@ -5,8 +5,8 @@ from modular_calc_jfy.percentage import Percentage
 @pytest.mark.parametrize("input_value,input_percentage,expected",
     [
         (100, 0.25, 125),
-        (12, 0.50, 18)
-        (50, 0.12, 56)
+        (12, 0.50, 18),
+        (50, 0.12, 56),
     ]
 )
 def test_add_percentage(input_value, input_percentage, expected):
@@ -15,8 +15,8 @@ def test_add_percentage(input_value, input_percentage, expected):
 @pytest.mark.parametrize("input_value,input_percentage,expected",
     [
         (125, 0.81, 226.25),
-        (15, 0.33, 10.05)
-        (50, 0.12, 44)
+        (15, 0.33, 10.05),
+        (50, 0.12, 44),
     ]
 )
 def test_sub_percentage(input_value, input_percentage, expected):
@@ -25,9 +25,8 @@ def test_sub_percentage(input_value, input_percentage, expected):
 @pytest.mark.parametrize("input_value,input_share,expected",
     [
         (100, 0.25, 25),
-        (50, 0.2, 10)
-        (80, 0.625, 5)
-    ]
+        (50, 0.2, 10),
+        (80, 0.625, 5),
 )
 def test_percentage_of(input_value, input_share, expected):
     assert pytest.approx(Percentage.percentage_of(input_value, input_share)) == expected
@@ -35,8 +34,8 @@ def test_percentage_of(input_value, input_share, expected):
 @pytest.mark.parametrize("input_value,input_w,expected_p",
     [
         (30, 15, 0.5),
-        (50, 10, 0.2)
-        (80, 5, 0.625)
+        (50, 10, 0.2),
+        (80, 5, 0.625),
     ]
 )
 def test_percentage(input_g, input_w, expected_p):
@@ -45,8 +44,8 @@ def test_percentage(input_g, input_w, expected_p):
 @pytest.mark.parametrize("input_net,expected",
     [
         (1000, 1190),
-        (250, 297.5)
-        (3.25, 3,87)
+        (250, 297.5),
+        (3.25, 3,87),
     ]
 )
 def test_gross(input_net,expected):
@@ -55,12 +54,10 @@ def test_gross(input_net,expected):
 @pytest.mark.parametrize("input_gross,expected",
     [
         (1000, 840.34),
-        (250, 210.08)
-        (16.5, 13,87)
+        (250, 210.08),
+        (16.5, 13,87),
     ]
 )
 def test_net(input_gross,expected):
     assert pytest.approx(Percentage.net(input_gross, input_w)) == expected_p
-
-
 
